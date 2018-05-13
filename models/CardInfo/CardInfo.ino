9d307f4c246f72ae624fee5665680ddb67b9aea0
I327500 <ward.peng@sap.com>
2018-05-13 21:32:48 +0800

    [new] Card info demo.

--- models/CardInfo/CardInfo.ino
+++ models/CardInfo/CardInfo.ino
@@ -1,24 +1,3 @@
-/*
-  SD card test 
-   
- This example shows how use the utility libraries on which the'
- SD library is based in order to get info about your SD card.
- Very useful for testing a card when you're not sure whether its working or not.
- 	
- The circuit:
-  * SD card attached to SPI bus as follows:
- ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
- ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
- ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
- ** CS - depends on your SD card shield or module. 
- 		Pin 4 used here for consistency with other Arduino examples
-
- 
- created  28 Mar 2011
- by Limor Fried 
- modified 9 Apr 2012
- by Tom Igoe
- */
  // include the SD library:
 #include <SD.h>
 
